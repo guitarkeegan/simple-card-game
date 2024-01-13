08895b5e5b4f4784445bd3e8751cd8ac116b9a57
guitarkeegan <keegananglim@gmail.com>
2024-01-13 13:30:54 -0800

    get user input

--- main.go
+++ main.go
@@ -1,4 +1,22 @@
 package main
 
-func main(){
-    }
+import (
+	"fmt"
+)
+
+func main() {
+	var userInput string
+
+	fmt.Print("Enter your input: ")
+	_, err := fmt.Scanln(&userInput)
+	if err != nil {
+		fmt.Println("Error reading input:", err)
+		return
+	}
+
+	if userInput == "rand" || userInput == "high" {
+		fmt.Printf("Great! lets play %s\n", userInput)
+	} else {
+		fmt.Println("Sorry, we don't have that one.")
+	}
+}
